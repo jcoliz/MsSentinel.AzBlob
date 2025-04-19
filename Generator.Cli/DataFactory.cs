833f9d66c3ec5320078f7f761d954a8a842e0ffd
James Coliz <jcoliz@outlook.com>
2025-04-18 18:15:04 -0700

    feat: Add web transaction

---
+++ Generator.Cli/DataFactory.cs
@@ -0,0 +1,9 @@
+namespace Generator.Cli;
+
+public static class DataFactory
+{
+    static IEnumerable<WebTransaction> CreateWebTransactions(int count)
+    {
+        return Enumerable.Empty<WebTransaction>();
+    }
+}

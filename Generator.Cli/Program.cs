a19a728a7cce1f46085f9e1dbe7025e44cc73e99
James Coliz <jcoliz@outlook.com>
2025-04-21 09:34:33 -0700

    feat: Upload to blob storage

--- Generator.Cli/Program.cs
+++ Generator.Cli/Program.cs
@@ -1,5 +1,20 @@
 ﻿using System.IO.Compression;
+using Azure.Storage.Blobs;
 using Generator.Cli;
+using Microsoft.Extensions.Configuration;
+
+//
+// Load configuration
+//
+
+var configuration = new ConfigurationBuilder()
+    .AddTomlFile("config.toml", optional: false, reloadOnChange: true)
+    .Build();
+
+BlobStorageOptions options = new();
+configuration.Bind(BlobStorageOptions.Section, options);
+
+var client = new BlobServiceClient(configuration.GetConnectionString("StorageAccount"));
 
 var data = DataFactory.CreateWebTransactions(10,TimeSpan.FromMinutes(2));
 
@@ -7,7 +22,8 @@ Directory.CreateDirectory("out");
 
 var session = Guid.NewGuid();
 var now = DateTimeOffset.UtcNow;
-var filename = $"out/txn-{now.ToUnixTimeMilliseconds()}-{session.ToString().Replace("-","")}.csv";
+var dataname = $"txn-{now.ToUnixTimeMilliseconds()}-{session.ToString().Replace("-","")}";
+var filename = $"out/{dataname}.csv";
 
 {
     using var stream = File.OpenWrite(filename);
@@ -23,8 +39,16 @@ var filename = $"out/txn-{now.ToUnixTimeMilliseconds()}-{session.ToString().Repl
     await writer.FlushAsync();
 }
 
-using FileStream originalFileStream = File.Open(filename, FileMode.Open);
-using FileStream compressedFileStream = File.Create(filename + ".gz");
-using var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
-await originalFileStream.CopyToAsync(compressor);
-await compressedFileStream.FlushAsync();
+{
+    using FileStream originalFileStream = File.Open(filename, FileMode.Open);
+    using FileStream compressedFileStream = File.Create(filename + ".gz");
+    using var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
+    await originalFileStream.CopyToAsync(compressor);
+    await compressedFileStream.FlushAsync();
+}
+
+var containerClient = client.GetBlobContainerClient(options.Container);
+await containerClient.CreateIfNotExistsAsync();
+
+var blobClient = containerClient.GetBlobClient($"{options.Folder}/{dataname}.csv.gz");
+await blobClient.UploadAsync(filename + ".gz");
